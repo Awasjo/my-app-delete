8c03106389c804e3f0940b0f7523fdcc0321c9c3
awasjo <awasjomail@gmail.com>
2024-02-01 15:47:23 -0500

    added conditioning for score table

--- src/components/ScoreInfoTable.tsx
+++ src/components/ScoreInfoTable.tsx
@@ -8,6 +8,15 @@ interface ScoreInfoTableProps {
 
 const ScoreInfoTable: React.FC<ScoreInfoTableProps> = ({phrase}) => {
 
+    // Define an array of keywords to search for
+    const keywords = ['Thank you', 'Promotion', 'Welcome'];
+
+    // Function to check if a keyword exists in the transcript
+    const hasKeyword = (text: string, keyword: string) => {
+      console.log("test")
+      console.log(text.toLowerCase().includes(keyword.toLowerCase()))
+      return text.toLowerCase().includes(keyword.toLowerCase());
+    };
   
   return (
     <div className="score-info-table">
@@ -24,7 +33,7 @@ const ScoreInfoTable: React.FC<ScoreInfoTableProps> = ({phrase}) => {
           <tr>
             <td>Thank you</td>
             <td>5</td>
-            <td className="placeholder">Placeholder</td>
+            <td className="placeholder">{hasKeyword(phrase,keywords[0])? "1 / 1": "0 / anana111"}</td>
             <td className="placeholder">Placeholder</td>
           </tr>
           <tr>
